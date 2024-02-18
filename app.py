21f51e2658039037b388e8c42aff4bacd56082c5
shmudivel <82043832+shmudivel@users.noreply.github.com>
2024-02-18 05:12:07 -0500

    version bug fixed

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from dotenv import load_dotenv
-import streamlit as st
-
 from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
-
 from langchain.vectorstores import Qdrant
 from langchain.embeddings.openai import OpenAIEmbeddings
 import qdrant_client
+from dotenv import load_dotenv
 import os
+import streamlit as st
 
 def get_vector_store():
+    
     client = qdrant_client.QdrantClient(
         os.getenv("QDRANT_HOST"),
         api_key = os.getenv("QDRANT_API_KEY")
     )
+
+        # Check connection
+    # try:
+    #     get_vector_store()
+    #     print("Connected to Qdrant server.")
+    # except Exception as e:
+    #     print("Failed to connect to Qdrant server.")
+    #     print(e)
 
     embeddings = OpenAIEmbeddings()
 
@@ -28,15 +35,16 @@ def get_vector_store():
 def main():
     load_dotenv()
 
-    st.set_page_config(page_title="Ask Qdrant", page_icon="📄")
+    st.set_page_config(page_title="Ask Qdrant")
     st.header("Ask you remote database")
 
     # create vector store
     vector_store = get_vector_store()
 
+
     # create the chain and retriever
     qa = RetrievalQA.from_chain_type(
-        llm=OpenAI(),
+        llm=OpenAI(model_name="gpt-3.5-turbo"),
         chain_type="stuff",
         retriever=vector_store.as_retriever()
     )
